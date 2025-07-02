3db4e8f2a5fcb7a754d5dc15f678f650e8633d7e
Vladimir Yazykov <neizerth@gmail.com>
2025-07-02 13:01:17 +0300

    props

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,35 @@
 import "./App.css";
 import { About } from "./components/About";
+import { Job } from "./components/Job";
+import { TaskList } from "./components/TaskList";
+
+const tasks = [
+  {
+    id: 1,
+    title: "Покормить кота",
+    deadline: "23:59",
+    completed: true,
+  },
+  {
+    id: 2,
+    deadline: "01-01-2026",
+    title: "Поспать 10 часов",
+  },
+];
 
 function App() {
-  return <About />;
+  return (
+    <>
+      <About />
+      <Job
+        title="Банк Африки"
+        post="Главный консультант"
+        description="Давал советы"
+      />
+      <Job title="Бар Чайка" post="Главная чайка" description="Орал чайкой" />
+      <TaskList tasks={tasks} />
+    </>
+  );
 }
 
 export default App;
